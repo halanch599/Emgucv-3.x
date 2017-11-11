f158a8b5e1abfb54344431a17300dd90f6f19460
Tech AJ <32822650+halanch599@users.noreply.github.com>
2017-11-11 20:41:46 +0300

    Create formContours.cs

---
+++ Object Contour Extraction/formContours.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+
+namespace Emgucv33Apps
+{
+    public partial class formContours : Form
+    {
+
+        Image<Bgr, byte> imgInput;
+
+        public formContours()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            if (ofd.ShowDialog()==DialogResult.OK)
+            {
+                imgInput = new Image<Bgr, byte>(ofd.FileName);
+                pictureBox1.Image = imgInput.Bitmap;
+            }
+        }
+
+        private void findContoursToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Image<Gray, byte> imgOutput = imgInput.Convert<Gray, byte>().ThresholdBinary(new Gray(100), new Gray(255));
+            Emgu.CV.Util.VectorOfVectorOfPoint contours = new Emgu.CV.Util.VectorOfVectorOfPoint();
+            Mat hier = new Mat();
+
+            Image<Gray, byte> imgout = new Image<Gray, byte>(imgInput.Width, imgInput.Height, new Gray(0));
+
+            CvInvoke.FindContours(imgOutput, contours, hier, Emgu.CV.CvEnum.RetrType.External, Emgu.CV.CvEnum.ChainApproxMethod.ChainApproxSimple);
+            CvInvoke.DrawContours(imgout, contours, -1, new MCvScalar(255, 0, 0));
+
+            pictureBox2.Image = imgout.Bitmap;
+
+        }
+    }
+}
