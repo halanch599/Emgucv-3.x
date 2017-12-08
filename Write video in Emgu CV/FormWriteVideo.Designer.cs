c9b2c073b2e7e6a8bbc5fb9d623cb79fcf7201c7
Tech AJ <32822650+halanch599@users.noreply.github.com>
2017-12-08 07:19:21 +0300

    Create FormWriteVideo.Designer.cs

---
+++ Write video in Emgu CV/FormWriteVideo.Designer.cs
@@ -0,0 +1,108 @@
+namespace Emgucv33Apps
+{
+    partial class FormWriteVideo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.readVideoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.writeVideoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(521, 374);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(521, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.readVideoToolStripMenuItem,
+            this.writeVideoToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // readVideoToolStripMenuItem
+            // 
+            this.readVideoToolStripMenuItem.Name = "readVideoToolStripMenuItem";
+            this.readVideoToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.readVideoToolStripMenuItem.Text = "Read Video";
+            this.readVideoToolStripMenuItem.Click += new System.EventHandler(this.readVideoToolStripMenuItem_Click);
+            // 
+            // writeVideoToolStripMenuItem
+            // 
+            this.writeVideoToolStripMenuItem.Name = "writeVideoToolStripMenuItem";
+            this.writeVideoToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.writeVideoToolStripMenuItem.Text = "Write Video";
+            this.writeVideoToolStripMenuItem.Click += new System.EventHandler(this.writeVideoToolStripMenuItem_Click);
+            // 
+            // FormWriteVideo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 398);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FormWriteVideo";
+            this.Text = "FormWriteVideo";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem readVideoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem writeVideoToolStripMenuItem;
+    }
+}
