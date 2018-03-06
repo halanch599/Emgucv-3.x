32706914fba3082901c2760d9ad3c4db9f94a823
Tech AJ <32822650+halanch599@users.noreply.github.com>
2018-03-06 06:09:49 +0300

    Create FormFaceDetection.Designer.cs

---
+++ EmguCV -28 Face Detection -Haar or LBP/FormFaceDetection.Designer.cs
@@ -0,0 +1,118 @@
+namespace Emgucv33Apps
+{
+    partial class FormFaceDetection
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.detectFaceHaarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.detectFaceLBPToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(795, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(795, 439);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.detectFaceHaarToolStripMenuItem,
+            this.detectFaceLBPToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(163, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // detectFaceHaarToolStripMenuItem
+            // 
+            this.detectFaceHaarToolStripMenuItem.Name = "detectFaceHaarToolStripMenuItem";
+            this.detectFaceHaarToolStripMenuItem.Size = new System.Drawing.Size(163, 22);
+            this.detectFaceHaarToolStripMenuItem.Text = "Detect Face Haar";
+            this.detectFaceHaarToolStripMenuItem.Click += new System.EventHandler(this.detectFaceHaarToolStripMenuItem_Click);
+            // 
+            // detectFaceLBPToolStripMenuItem
+            // 
+            this.detectFaceLBPToolStripMenuItem.Name = "detectFaceLBPToolStripMenuItem";
+            this.detectFaceLBPToolStripMenuItem.Size = new System.Drawing.Size(163, 22);
+            this.detectFaceLBPToolStripMenuItem.Text = "Detect Face LBP";
+            this.detectFaceLBPToolStripMenuItem.Click += new System.EventHandler(this.detectFaceLBPToolStripMenuItem_Click);
+            // 
+            // FormFaceDetection
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(795, 463);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FormFaceDetection";
+            this.Text = "FormFaceDetection";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem detectFaceHaarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem detectFaceLBPToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
